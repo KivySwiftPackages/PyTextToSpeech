457a3a92fe2400aab97949fa23e6fa8655052030
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2025-08-13 10:25:34 +0200

    Update PySwiftKit repository URL in Package.swift
    Changed the PySwiftKit dependency URL to use the new repository location at https://github.com/Py-Swift/PySwiftKit. This ensures the package references the correct upstream source.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 		.library(name: "PyTextToSpeech", targets: ["PyTextToSpeech"])
 	],
 	dependencies: [
-        .package(url: "https://github.com/PythonSwiftLink/PySwiftKit", from: .init(311, 0, 0)),
+        .package(url: "https://github.com/Py-Swift/PySwiftKit", from: .init(311, 0, 0)),
         //.package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", from: .init(0, 0, 0)),
 	],
 	targets: [
